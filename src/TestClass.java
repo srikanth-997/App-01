3466bae76b19cbce7d88bc6199c6ab4e66c748b4
srikanth.N <srikanthn916@gmail.com>
2022-02-21 14:13:14 +0530

    this is feature1 branch code

--- src/TestClass.java
+++ src/TestClass.java
@@ -7,6 +7,11 @@ public class TestClass {
                 System.out.println("Welcome");
                 System.out.println("Github");
                 System.out.println("This is my code");
+                int a=10;
+                int b=20;
+                int res=a+b;
+                System.out.println(res);
+               
 
 	}
 
